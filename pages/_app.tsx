90e08356651d793d77728d11b66fa1302cd64536
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-11 15:19:27 +0300

    SSR check for less erros

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+// in next.js here we create the structure
 import type { AppProps } from 'next/app'
+import { useState, useEffect } from 'react'
+import '../styles/globals.css'
 
+
+// starting with asuming SSR server side rendered page 
+// useEffect runs at the start to see when it is not SSR
+// this check helps in smoother workflow
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isSSR, setisSSR] = useState(true);
+
+  useEffect(() => {
+    setisSSR(false);
+  },[]);
+
+  if(isSSR) return null;
+
   return <Component {...pageProps} />
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='text-3xl font-bold underline'>
       TikTik
     </div>
   )
