049b1d653973216ea816de69c1c48a00105f1398
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-11 19:44:19 +0300

    Style of icon list in Discover

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -7,6 +7,11 @@ import { topics } from '../utils/constants';
 
 
 const Discover = () => {
+
+  const activeTopicStyle = ''
+
+  const topicStyle = 'xl:border-2 hover:bg-primary xl:border=gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black'
+
   return (
     <div className='xl:border-b-2 xl:border-gray-200 pb-6'>
       <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>
@@ -16,8 +21,8 @@ const Discover = () => {
       <div className='flex gap-3 flex-wrap'>
         {topics.map((item) =>(
           <Link href={`/?topics=${item.name}`} key={item.name}>
-            <div>
-              <span className='font-bold text=2xl xl:text-md'>
+            <div className={topicStyle}>
+              <span className='font-bold text-2xl xl:text-md'>
                 {item.icon}
               </span>
               <span className='font-medium text-md hidden xl:block capitalize'>
