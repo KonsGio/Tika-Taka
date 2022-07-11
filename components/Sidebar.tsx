5bd0d7bf2330033b46920f704c284e19b7ddd8bf
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-11 18:07:41 +0300

    Imported libraries for Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import GoogleLogin from 'react-google-login';
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
+import { ImCancelCircle } from 'react-icons/im';
 
 const Sidebar = () => {
   return (
