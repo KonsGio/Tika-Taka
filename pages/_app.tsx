68dc7ca75b8ee09743c31277d1f25c273d3c6b30
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-11 15:29:02 +0300

    Imported Navbar,Sidebar to app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,8 @@
 import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react'
 import '../styles/globals.css'
-
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
 
 // starting with asuming SSR server side rendered page 
 // useEffect runs at the start to see when it is not SSR
@@ -18,10 +19,11 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <div>
+      <Navbar/>
       {/* we use tailwind -> styles inside jsx */}
       <div className='flex gap-6 md:gap-20'>
         <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
-          Sidebar
+          <Sidebar/>
         </div>
         <div className='mt-4 flex flex-col gap-10 overflow-auto h-[84vh] videos flex-1'>
           <Component {...pageProps} />
