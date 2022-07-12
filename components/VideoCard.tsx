f57d16d18f01fda859f6b33d4122c318671e7647
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-12 16:28:08 +0300

    Creating IProps in VideoCard.tsx

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { Video } from '../types';
+// 1. Once again we need to create IProps in tsx
+interface IProps {
+    post:Video;
+}
 
 const VideoCard = () => {
   return (
