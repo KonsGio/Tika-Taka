9b02ef74e938112641f563607d6bd531cc01f0bd
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-12 14:52:27 +0300

    Fetching data from our own backend - api/index.ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 // we fetch data-videos from sanity
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import axios from 'axios';
+
 
 
 const Home: NextPage = () => {
@@ -9,5 +11,19 @@ const Home: NextPage = () => {
     </div>
   )
 }
+
+//To fetch data we use getServerSideProps which will side server render the page and used only if we need to render a page whose data must be fetched at requested time
+// We will be fetching videos each time we reload the page
+export const getServerSideProps = async () => {
+  // api request to our own backend
+  const response = await axios.get(`http://localhost:3000/api/post`);
+
+  console.log(response.data.name);
+
+  return{
+    props:{}
+  }
+}
+
 
 export default Home
