b8f43d0f03eca5e6f41de7cc1418d76869550a18
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-12 16:30:34 +0300

    Specify type of post.something, returns errors and suitable props

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Video } from '../types';
+// 2.To specify a type
+import {NextPage} from 'next';
+
 // 1. Once again we need to create IProps in tsx
 interface IProps {
     post:Video;
 }
 
-const VideoCard = () => {
+const VideoCard:NextPage<IProps> =  ({post}) => {
   return (
     <div>VideoCard</div>
   )
