bd93c7bdf08220187de78c23b130801527a6197c
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-13 18:47:07 +0300

    Libraries for video control and buttons

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+// imports for video buttons and controls
+import Link from 'next/link';
+import {HiVolumeUp,HiVolumeOff} from 'react-icons/hi';
+import { BsPlay,BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
+import { useEffect, useRef, useState } from 'react';
+import {GoVerified} from 'react-icons/go';
+ 
+
+
 import { Video } from '../types';
 // 2.To specify a type
 import {NextPage} from 'next';
+
+
+
+
 
 // 1. Once again we need to create IProps in tsx
 interface IProps {
