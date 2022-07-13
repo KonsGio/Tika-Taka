c592907356aecc0dfdbaa37da5e65c56e0c1f393
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-13 19:07:25 +0300

    User profile photo added on video card

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,10 +1,11 @@
-// imports for video buttons and controls
+// 4. Imports for video buttons and controls
 import Link from 'next/link';
 import {HiVolumeUp,HiVolumeOff} from 'react-icons/hi';
 import { BsPlay,BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
 import { useEffect, useRef, useState } from 'react';
 import {GoVerified} from 'react-icons/go';
- 
+import Image from 'next/image';
+
 
 
 import { Video } from '../types';
@@ -22,7 +23,28 @@ interface IProps {
 // 2.To specify a type
 const VideoCard:NextPage<IProps> =  ({post}) => {
   return (
-    <div>VideoCard</div>
+    <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
+      <div>
+        <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded'>
+          <div className='md:w-16 md:h-16 w-10 h-10'>
+            <Link href='/'>
+              {/* 5. You cant put an image as a child component of a link  thats why
+              we create a new empty react fragment to put image inside */}
+              <>
+                <Image 
+                  width={62}
+                  height={62}
+                  className='rounded-full'
+                  src={post.postedBy.image}
+                  alt='profile photo'
+                  layout='responsive'
+                />
+              </>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+      // Link found in error page under image hostname add this here
+      // images:{
+      //   domains:['avatars.githubusercontent.com']
+      // }
+
+  images:{
+    domains:['avatars.githubusercontent.com']
+  }
 }
 
 module.exports = nextConfig
