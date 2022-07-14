930d6c55c2b20f295c58b0a075e5cc9cbded6c35
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-14 16:31:30 +0300

    User object creation. With data fetched from google log in

--- utils/index.ts
+++ utils/index.ts
@@ -2,7 +2,19 @@ import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 
 export const createOrGetUser = async (response: any) => {
-  const decoded = jwt_decode(response.credential)
-  
+
+
+// We deconstructed the data that we take from google log in fetched object
+// And we assign these data to user const
+  const decoded:{ name : string, picture: string, sub:string} = jwt_decode(response.credential)
+  const { name, picture, sub} = decoded;
+  const user = {
+    _id:sub,
+    _type:'user',
+    userName: name,
+    image: picture,
+// This was the user object creation
+  }
+
   console.log(decoded);
 };
