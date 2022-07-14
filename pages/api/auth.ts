2aefd021efac9cca6e346f947659803db0835f67
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-14 17:25:33 +0300

    Google login adds new user to database

---
+++ pages/api/auth.ts
@@ -0,0 +1,21 @@
+// This is our server
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { client } from '../../utils/client';
+
+// req:NextApiRequest means we are using typescript
+// Inside NextApiRequest we see what it prints
+export default async function handler(req: NextApiRequest,res: NextApiResponse) {
+    // Its route can be any of the HTTP types post,get etc
+  if(req.method === 'POST'){
+    
+    //to get document 
+    const user = req.body;
+    // call sanity client
+    client.createIfNotExists(user)
+    .then(()=> res.status(200).json('Login successful'))
+    
+  }
+}
+
+    // #####THIS PROCEDURE WILL ADD NEW USER IN OUR SANITY DATABASE#####
+
--- utils/index.ts
+++ utils/index.ts
@@ -4,17 +4,20 @@ import jwt_decode from 'jwt-decode';
 export const createOrGetUser = async (response: any) => {
 
 
-// We deconstructed the data that we take from google log in fetched object
-// And we assign these data to user const
-  const decoded:{ name : string, picture: string, sub:string} = jwt_decode(response.credential)
+//1. We deconstructed the data that we take from google log in fetched object
+//2. And we assign these data to user const
+  const decoded:{ name : string, picture: string, sub:string} = jwt_decode(response.credential);
+ 
   const { name, picture, sub} = decoded;
+ 
   const user = {
     _id:sub,
     _type:'user',
     userName: name,
-    image: picture,
-// This was the user object creation
+    image: picture
+//3. This was the user object creation
   }
-
-  console.log(decoded);
+//4. We make an API call with axios
+// 5. we are passing the user data and now we will create the API route at api/auth.ts 
+await axios.post('hhtp://localhost:3000/api/auth', user);
 };
