1f3f98bbf2db1324b48245fdc830e3a6ed366ece
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-14 15:18:26 +0300

    Deleted uused libraries from VideoCard

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,21 +1,11 @@
 // 4. Imports for video buttons and controls
 import Link from 'next/link';
-import {HiVolumeUp,HiVolumeOff} from 'react-icons/hi';
-import { BsPlay,BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
-import { useEffect, useRef, useState } from 'react';
+import {useRef, useState } from 'react';
 import {GoVerified} from 'react-icons/go';
 import Image from 'next/image';
-
-
-
 import { Video } from '../types';
 // 2.To specify a type
 import {NextPage} from 'next';
-
-
-
-
-
 // 1. Once again we need to create IProps in tsx
 interface IProps {
     post:Video;
