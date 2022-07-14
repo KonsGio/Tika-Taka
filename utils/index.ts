bdc8e63333a5ed0f89219a0cc1b5c0bf2f650bf2
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-14 16:25:59 +0300

    Google sign in fetches an object with all user data

--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
-
+import jwt_decode from 'jwt-decode';
 
 export const createOrGetUser = async (response: any) => {
-  console.log(response.credential)
+  const decoded = jwt_decode(response.credential)
   
-  
-
+  console.log(decoded);
 };
