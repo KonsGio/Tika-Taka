d2d07c357c6136fae3f5da11cc2166c425c2f00a
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-15 15:06:30 +0300

    Upload page landing page complete with no functionality

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -10,6 +10,9 @@ import { SanityAssetDocument } from '@sanity/client';
 import useAuthStore from '../store/authStore';
 import { client } from '../utils/client';
 
+// Import all topics
+import { topics } from '../utils/constants';
+
 
 const Upload = () => {
     const [isloading, setIsloading] = useState(false);
@@ -42,8 +45,8 @@ const Upload = () => {
     }
  
   return (
-    <div className='flex w-full h-full'>
-        <div className='bg-white rounded-lg'>
+    <div className='flex w-full h-full absolute left-0 top-[60px] mb-10 pt-10 lg:pt-20 bg-[#f8f8f8] justify-center'>
+        <div className='bg-white rounded-lg cl:h-[80vh] w-[60%] flex gap-6 flex-wrap justify-between items-center p-14 pt-6'>
             <div>
                 <div>
                     <p className='text-2xl font-bold'>Upload Video</p>
@@ -98,7 +101,55 @@ const Upload = () => {
                                 )}
                             </div>
                         )}
+                        {wrongFileType && (
+                            <p className='text-center text-xl text-red-400 font-semibold mt-4 w-[250px]'>
+                                Please select a supported video file.
+                            </p>
+                        )}
                 </div>
+            </div>
+            <div className='flex flex-col gap-3 pb-10'>
+                 <label className='text-md font-medium'>Caption</label>
+                 <input
+                    type='text'
+                    value=''
+                    onChange={() => {}}
+                    className='rounded outline-none text-md border-2 border-gray-200 p-2'
+                 />
+                    <label className='flex flex-col gap-3 pb-10'>
+                        Choose a category
+                    </label>
+                    <select
+                    onChange={() => {}}
+                    className='outline-none border-2 border-gray-200 text-md capitalize lg:p-4 p-2 rounded cursor-pointer'
+                    >
+                        {/* Loop all categories when we loop somethin we need a key so react can differentiate it*/}
+                        {topics.map((topic) => (
+                            <option
+                            key={topic.name}
+                            className='outline-none capitalize bg-white text-gray-700 text-md p-2 hover:bg-slate-300'
+                            value={topic.name}
+                            >
+                                {topic.name}
+                            </option>
+                        ))}
+                    </select>
+                    <div className='flex gap-6 mt-10'>
+                            <button
+                            onClick={() => {}}
+                            type='button'
+                            className='border-gray-300 border-2 text-md font-medium p-2 rounded w-28 lg:w-44 outline-none'
+                            >
+                                Discard
+                            </button>
+                            <button
+                            onClick={() => {}}
+                            type='button'
+                            className='bg-[#9A100E] text-white text-md font-medium p-2 rounded w-28 lg:w-44 outline-none'
+                            >
+                                Post
+                            </button>
+                    </div>
             </div>
         </div>
     </div>
