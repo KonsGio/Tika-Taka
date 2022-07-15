5904db5bbee66c2241dcf36606f74aa0aaae8c17
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-15 17:54:47 +0300

    Basic video page structure and functioning play and back button

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,12 +1,11 @@
 // THis file's url is dynamic [id] it will change for every single video
-import React, {useState,useEffect,useRef} from 'react';
+import React, {useState,useRef} from 'react';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Link from 'next/link';
 import { GoVerified } from 'react-icons/go';
-import { MdOutlineCancel } from 'react-icons/md';
+import { MdOutlineArrowBack} from 'react-icons/md';
 import { BsFillPlayFill } from 'react-icons/bs';
-import { HiVolumeUp, HiVolumeOff} from 'react-icons/hi';
 import axios from 'axios';
 import { BASE_URL } from '../../utils';
 import { Video } from '../../types';
@@ -19,9 +18,42 @@ interface IProps {
 }
 
 const Detail = ({postDetails} : IProps) => {
-  return (
-    <div>
+  
+  const [post, setPost] = useState(postDetails);
 
+  const videoRef = useRef(null);
+
+  const router = useRouter();
+
+  if(!post) return null;
+
+  return (
+    // Tailwind classes
+    <div className='flex w-full
+     absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
+        <div className='relative flex-2 w-[1000px] lg:w-9/12 
+                        flex justify-center items-center bg-black'>
+              <div className='absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
+                <p className='cursor-pointer'
+                  onClick={() => router.back()}
+                >
+                  <MdOutlineArrowBack className='text-white text-[35px]'/>
+                </p>
+              </div>
+              <div className='relative '>
+                <div className='lg:h-[100vh] h-[60vh]'>
+                  <video 
+                  ref={videoRef}
+                  loop
+                  controls
+                  onClick={()=>{}}
+                  src={post.video.asset.url}
+                  className='h-full cursor-pointer'
+                  >
+                  </video>
+                </div>
+              </div>
+        </div>
     </div>
   )
 }
