da3cde67de8558e720e0c5031e764a4a42ce613e
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-15 23:01:40 +0300

    Showing Results in comments section

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
-// 1.We create IProps
+import React from 'react';
+import {MdOutlineVideocamOff} from 'react-icons/md';
+import { BiCommentX } from 'react-icons/bi';
 
+
+// 1.We create IProps
 interface IProps{
     text:string;
 }
@@ -8,7 +11,16 @@ interface IProps{
 // 2.Second way to specify type destucturing text 
 const NoResults = ({text} : IProps) => {
   return (
-    <div>NoResults</div>
+    <div className='flex flex-col justify-center items-center h-full w-full'>
+        <p className='text-8xl'>
+          {
+            text === 'No comments yet!' 
+            ? <BiCommentX/> 
+            : <MdOutlineVideocamOff />
+          }
+        </p>
+        <p className='text-2xl text-center'>{text}</p>
+    </div>
   )
 }
 
