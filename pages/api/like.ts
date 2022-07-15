082ba176b86e1e77b7141316f1f85a8914ab8aa9
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-15 22:28:28 +0300

    Exports like res

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -23,7 +23,7 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse) 
         .unset([`likes[_ref=="${userId}"]`])
         .commit();
 
-        res.status(200).json
+        res.status(200).json(data);
   }
 }
 
