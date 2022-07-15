2a307267f6b5ee2ffd85555ed22f8df88d47bc32
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-15 16:42:39 +0300

    Dynamic video page added in file tree with basic structure

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,10 +1,33 @@
-// THis files url is dynamic [id] it will change for every single video
-import React from 'react'
+// THis file's url is dynamic [id] it will change for every single video
+import React, {useState,useEffect,useRef} from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import Link from 'next/link';
+import { GoVerified } from 'react-icons/go';
+import { MdOutlineCancel } from 'react-icons/md';
+import { BsFillPlayFill } from 'react-icons/bs';
+import { HiVolumeUp, HiVolumeOff} from 'react-icons/hi';
+import axios from 'axios';
+import { BASE_URL } from '../../utils';
+
+
+// We will fetch our data by using the id inside of the url
 
 const Detail = () => {
   return (
-    <div>Detail</div>
+    <div>
+
+    </div>
   )
+}
+
+export const getServerSideProps = async ({params: {id}
+}) => {
+  const { data } = await axios.get(`${BASE_URL}/api/post/${id}`)
+
+  return {
+    props: { postDetails : data }
+  }
 }
 
 export default Detail
