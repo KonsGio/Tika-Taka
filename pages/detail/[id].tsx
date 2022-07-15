7d30b6df63aba49b78d56153273e56d6259486f2
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-15 16:02:02 +0300

    Adding video details page in [id].ts

---
+++ pages/detail/[id].tsx
@@ -0,0 +1,10 @@
+// THis files url is dynamic [id] it will change for every single video
+import React from 'react'
+
+const Detail = () => {
+  return (
+    <div>Detail</div>
+  )
+}
+
+export default Detail
