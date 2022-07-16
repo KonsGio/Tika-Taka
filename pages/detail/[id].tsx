de3c890e3f842e3c62d0c5302afa80d73f3dabf7
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-16 20:23:28 +0300

    Commnet section updated every new comment

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -30,6 +30,12 @@ const Detail = ({postDetails} : IProps) => {
 
   const {userProfile} : any = useAuthStore();
 
+  const [comment, setComment] = useState('');
+
+  const [isPostingComment, setIsPostingComment] = useState(false);
+
+
+
   const handleLike = async (like:boolean) => {
     if(userProfile){
       const {data} = await axios.put(`${BASE_URL}/api/like`, {
@@ -43,7 +49,34 @@ const Detail = ({postDetails} : IProps) => {
       setPost({ ...post, likes: data.likes});
     }
   }
-  
+
+
+
+
+  // Asyncronous add comment function because it is going to take some time to execute
+  const addComment = async (e) => {
+    // We add prevent Default so that website doesnt reload
+    e.preventDefault();
+
+    // Is the user loged in or a comment exist?
+    if (userProfile && comment) {
+      setIsPostingComment(true);
+
+      const data = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+        userId:userProfile._id,
+        comment
+      });
+
+      setPost({ ...post, comments: data.comments});
+      setComment('');
+      setIsPostingComment(false);
+    }
+  }
+
+
+
+
+
   if(!post) return null;
 
   return (
@@ -122,13 +155,19 @@ const Detail = ({postDetails} : IProps) => {
                 )}
               </div>
               <Comments 
-              
+              comment={comment}
+              setComment={setComment}
+              addComment={addComment}
+              comments={post.comments}
+              idPostingComment={isPostingComment}
               />
           </div>
         </div>
     </div>
   )
 }
+
+
 
 export const getServerSideProps = async ({params: {id}
 } : {
