7eaf51626f3bd60f15d8732897faae6e1e2c3cbd
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-16 22:03:27 +0300

    Posted by property changed

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -27,7 +27,7 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse) 
             {
                 comment,
                 _key:v4(),
-                postedBy: {_type:'PostedBy', _ref: userId}
+                postedBy: {_type:'postedBy', _ref: userId}
             }
         ])
         .commit()
