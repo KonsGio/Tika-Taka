6b84b617ee6d94d02fa325a758017add304c60f0
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-16 20:22:35 +0300

    Comment upload PUT method

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { client } from '../../../utils/client';
 import { postDetailQuery } from '../../../utils/queries';
+import { v4 } from 'uuid';
 
 
 export default async function handler(req: NextApiRequest,res: NextApiResponse) {
@@ -15,7 +16,25 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse) 
     res.status(200).json(data[0]);
 
 
+  }else if (req.method === 'PUT') {
+    const { comment, userId } = req.body;
+    const { id } : any = req.query;
+
+    const data = await client
+        .patch(id)
+        .setIfMissing({comments:[]})
+        .insert('after', 'comments[-1]',[
+            {
+                comment,
+                _key:v4(),
+                postedBy: {_type:'PostedBy', _ref: userId}
+            }
+        ])
+        .commit()
+
+        res.status(200).json(data);
 }
+
 }
 
 
