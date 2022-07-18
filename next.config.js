e2a978b8bf4aca489657ecbc6978006d448d78e8
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-18 20:23:14 +0300

    Change next config

--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const nextConfig = {
     ignoreBuildErrors:true,
   },
   reactStrictMode: true,
-  swcMinify: true,
-      // Link found in error page under image hostname add this here
-      // images:{
-      //   domains:['avatars.githubusercontent.com']
-      // }
 
   images:{
     domains:['avatars.githubusercontent.com',
