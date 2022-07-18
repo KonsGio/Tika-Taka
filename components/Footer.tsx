f64ec4eae0966a2bb8aef537b68adbba54956d83
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-18 19:56:27 +0300

    Slice removed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
+import { NextPage } from 'next';
 import { footer} from '../utils/constants';
 
 
-  const Footer = () => {
-
-  return (
+const Footer: NextPage = () => (
+ 
     <div className='mt-6 hidden xl:block text-gray-500 text-md '>
       <p>
         Click to enter {footer}
       </p> 
      <p className='text-gray-500 text-sm mt-5'>2022 Tika Taka Created by Konstantinos Giovanitsas</p>
     </div>
-  )
-}
+
+)
 
 export default Footer
